dcc6092dae1e2f8876c9c6315b31c7e8c9d798fb
Aluno <adrielestoller@gmail.com>
2022-12-09 18:21:21 -0300

    Arquivos Caio - 09/12

---
+++ Subway Surfers (ai papai)/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController controller;
+    public float velocidade, alturaPulo, velocidadePulo, gravidade;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
