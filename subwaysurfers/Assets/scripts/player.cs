6b8a1acf8b7333a131463732229febfdb2a776d3
Adriel Estoller <adrielestoller@gmail.com>
2022-12-09 18:33:10 -0300

    Arquivos Gabriel - 09/12

---
+++ subwaysurfers/Assets/scripts/player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private CharacterController controller;
+    public float AlturaPulo, velocidadePulo, velocidade, gravidade;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
