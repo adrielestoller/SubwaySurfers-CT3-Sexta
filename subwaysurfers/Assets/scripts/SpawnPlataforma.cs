6dd27269e0117537301c030a7e7a6f207fdb49e3
Aluno <adrielestoller@gmail.com>
2022-12-16 18:30:05 -0300

    Arquivos Lucas - 16/12/2022

---
+++ subwaysurfers/Assets/scripts/SpawnPlataforma.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlataforma : MonoBehaviour
+{
+    public GameObject[] plataformas;
+
+    public float gerador = 0f;
+
+    private float tamanhoPlataforma = 30;
+
+    private Transform Jogador;
+
+    private int plataformasNaTela = 5;
+
+    private List<GameObject> plataformasAtivas = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Jogador = GameObject.FindGameObjectWithTag("Player").transform;
+
+        for (int i = 0; i <= plataformasNaTela; i++)
+            gerarPlataformas(Random.Range(0, i));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Jogador.position.z - 35 > (gerador - plataformasNaTela * tamanhoPlataforma))
+        {
+            gerarPlataformas(Random.Range(0, plataformas.Length));
+            Deletarplataformas();
+        }
+    }
+
+    public void gerarPlataformas(int numeroDaPista)
+    {
+        GameObject Iniciar;
+        Iniciar = Instantiate(plataformas[numeroDaPista]) as GameObject;
+        Iniciar.transform.SetParent(transform);
+        Iniciar.transform.position = Vector3.forward * gerador;
+        gerador += tamanhoPlataforma;
+        plataformasAtivas.Add(Iniciar);
+    }
+
+    public void Deletarplataformas()
+    {
+        Destroy(plataformasAtivas[0]);
+        plataformasAtivas.RemoveAt(0);
+    }
+}
---
+++ subwaysurfers/Assets/scripts/player.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private CharacterController controller;
+    public float AlturaPulo,
+        velocidadePulo,
+        velocidade,
+        alturaPulo,
+        gravidade;
+    private float posicaoHorizontal = 0;
+    private Vector3 Horizontal;
+    public float velocidadeDeTrocaDeRota = 100f;
+    private bool limite,
+        direita,
+        esquerda;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        direita = true;
+        esquerda = true;
+        limite = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direcao = Vector3.forward * velocidade;
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                velocidadePulo = alturaPulo;
+            }
+        }
+        else
+        {
+            velocidadePulo -= gravidade;
+        }
+
+        direcao.y = velocidadePulo;
+        controller.Move(direcao * Time.deltaTime);
+        Mover();
+    }
+
+    void Mover()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) && esquerda)
+        {
+            if (posicaoHorizontal == 0)
+            {
+                posicaoHorizontal += 3.5f;
+                limite = true;
+                if (posicaoHorizontal == -3.5f && limite)
+                {
+                    esquerda = false;
+                    direita = true;
+                }
+            }
+            else if (posicaoHorizontal == 3.5f)
+            {
+                posicaoHorizontal = 0;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) && direita)
+        {
+            if (posicaoHorizontal == 0)
+            {
+                posicaoHorizontal = 3.5f;
+                limite = true;
+                if (posicaoHorizontal == 3.5f && limite)
+                {
+                    direita = false;
+                    esquerda = true;
+                }
+            }
+            else if (posicaoHorizontal == -3.5f)
+            {
+                posicaoHorizontal = 0;
+            }
+        }
+        else
+        {
+            esquerda = true;
+            direita = true;
+            limite = false;
+        }
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            new Vector3(posicaoHorizontal, transform.position.y, transform.position.z),
+            velocidadeDeTrocaDeRota * Time.deltaTime
+        );
+    }
+}
